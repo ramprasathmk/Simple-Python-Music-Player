23520fbd99c70e7567f2817dc7827eb05749da3d
ramprasathmk <mkramprasath12@gmail.com>
2024-11-12 21:21:05 +0545

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,42 +4,56 @@ from os import walk
 from pygame import mixer
 from tkinter.filedialog import askdirectory
 
+# default-version-value
+version: str = "1.2.4"
 
-VER = "1.2.4"   # default-version-value
-Font = ("SourceCodePro-Medium", 14)  # use your own font to get better interface experience
-GUI_Window = Tk()
-GUI_Window.title(f"Simple Python Music Player v{VER}")
+# use your own font to get better interface experience
+Font: tuple[str, int] = ("SourceCodePro-Medium", 14)
+
+# root window
+GUI_Window: Tk = Tk()
+GUI_Window.title(f"Simple Python Music Player v{version}")
 # default-Dimension: 550x600
 GUI_Window.geometry('550x450')
 # not resizable
 GUI_Window.resizable(0, 0)  # type: ignore
 GUI_Window.config(bg="black")
 
-rootpath = askdirectory()  #  Select the Folder
-pattern = "*.mp3"
+# Select the Folder
+rootpath = askdirectory()
+
+# .mp3 pattern
+pattern: str = "*.mp3"
 
 mixer.init()
 
+# load the GUI Icon images
+prev_img: PhotoImage = PhotoImage(file="prev_img.png")
+stop_img: PhotoImage = PhotoImage(file="stop_img.png")
+play_img: PhotoImage = PhotoImage(file="play_img.png")
+paus_img: PhotoImage = PhotoImage(file="pause_img.png")
+next_img: PhotoImage = PhotoImage(file="next_img.png")
 
-prev_img = PhotoImage(file="prev_img.png")
-stop_img = PhotoImage(file="stop_img.png")
-play_img = PhotoImage(file="play_img.png")
-paus_img = PhotoImage(file="pause_img.png")
-next_img = PhotoImage(file="next_img.png")
 
+def select() -> None:
+    """
+    select() -> None: Select the Music directory
 
-def select():
+    play() -> None: Play the selected song
+    """
     label.config(text=listBox.get("anchor"))
     mixer.music.load(rootpath + "\\" + listBox.get("anchor"))
     mixer.music.play()
 
 
-def stop():
+def stop() -> None:
+    """stop() -> None: Stop the currently playing song """
     mixer.music.stop()
     listBox.select_clear('active')
 
 
-def play_next():
+def play_next() -> None:
+    """play_next() -> None: Play the next song """
     next_song = listBox.curselection()
     next_song = next_song[0] + 1
     next_song_name = listBox.get(next_song)
@@ -53,7 +67,8 @@ def play_next():
     listBox.select_set(next_song)
 
 
-def play_prev():
+def play_prev() -> None:
+    """play_prev() -> None: Play the previous song """
     next_song = listBox.curselection()
     next_song = next_song[0] - 1
     next_song_name = listBox.get(next_song)
@@ -67,7 +82,8 @@ def play_prev():
     listBox.select_set(next_song)
 
 
-def pause_song():
+def pause_song() -> None:
+    """Pause_song() -> None: Pause the currently playing song """
     if PauseButton['text'] == "Pause":
         mixer.music.pause()
         PauseButton['text'] = "Play"
@@ -76,42 +92,35 @@ def pause_song():
         PauseButton['text'] = "Pause"
 
 
-listBox = Listbox(GUI_Window, fg="red", bg="black", width=100, font=Font)
+listBox: Listbox = Listbox(GUI_Window, fg="red", bg="black", width=100, font=Font)
 listBox.pack(padx=15, pady=15)
 
-
-label = Label(GUI_Window, text="", bg="black", fg="yellow", font=Font)
+label: Label = Label(GUI_Window, text="", bg="black", fg="yellow", font=Font)
 label.pack(pady=15)
 
-
-top = Frame(GUI_Window, bg="black")
+top: Frame = Frame(GUI_Window, bg="black")
 top.pack(padx=10, pady=5, anchor='center')
 
+prevButton: Button = Button(GUI_Window, text="Prev", image=prev_img, bg="black", borderwidth=0, command=play_prev)
+prevButton.pack(pady=15, in_=top, side='left')
 
-prevButton = Button(GUI_Window, text="Prev", image=prev_img, bg="black", borderwidth=0, command=play_prev)
-prevButton.pack(pady=15, in_ = top, side='left')
+StopButton: Button = Button(GUI_Window, text="Stop", image=stop_img, bg="black", borderwidth=0, command=stop)
+StopButton.pack(pady=15, in_=top, side='left')
 
+PlayButton: Button = Button(GUI_Window, text="Play", image=play_img, bg="black", borderwidth=0, command=select)
+PlayButton.pack(pady=15, in_=top, side='left')
 
-StopButton = Button(GUI_Window, text="Stop", image=stop_img, bg="black", borderwidth=0, command=stop)
-StopButton.pack(pady=15, in_ = top, side='left')
+PauseButton: Button = Button(GUI_Window, text="Pause", image=paus_img, bg="black", borderwidth=0, command=pause_song)
+PauseButton.pack(pady=15, in_=top, side='left')
 
-
-PlayButton = Button(GUI_Window, text="Play", image=play_img, bg="black", borderwidth=0, command=select)
-PlayButton.pack(pady=15, in_ = top, side='left')
-
-
-PauseButton = Button(GUI_Window, text="Pause", image=paus_img, bg="black", borderwidth=0, command=pause_song)
-PauseButton.pack(pady=15, in_ = top, side='left')
-
-
-NextButton = Button(GUI_Window, text="Next", image=next_img, bg="black", borderwidth=0, command=play_next)
-NextButton.pack(pady=15, in_ = top, side='left')
+NextButton: Button = Button(GUI_Window, text="Next", image=next_img, bg="black", borderwidth=0, command=play_next)
+NextButton.pack(pady=15, in_=top, side='left')
 
 
-def main():
+def main() -> None:
     for root, dirs, files in walk(rootpath):
         for filename in fnmatch.filter(files, pattern):
-                listBox.insert('end', filename)
+            listBox.insert('end', filename)
 
     GUI_Window.mainloop()
 
